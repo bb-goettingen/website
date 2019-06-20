49b8a80e37eeeedadb304171c7d44d01446facee
Emily <etacarinaea@protonmail.com>
2019-06-20 17:34:41 +0200

    Insert some text from old website

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,11 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <p>
+      Das Basisdemokratische Bündnis ist eine Gruppe, in der sich
+      Einzelpersonen und diverse Basisgruppen und Fachgruppen zusammengefunden
+      haben, um gemeinsam emanzipatorische Politik an der Uni voran zu bringen.
+    </p>
   </Layout>
 )
 
