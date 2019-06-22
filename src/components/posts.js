93573b1feaef7e03138d10559c0adc084762e119
Emily <etacarinaea@protonmail.com>
2019-06-22 15:58:27 +0200

    Add stylesheet for postlist

--- src/components/posts.js
+++ src/components/posts.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import "./posts.scss"
+
 const PostShort = ({ node }) => {
   const { excerpt, frontmatter: fm } = node;
   // /\n+(?!#)/g : match new lines unless followed by a heading (#)
@@ -9,10 +11,12 @@ const PostShort = ({ node }) => {
                               .split("\n")
                               .map(line => <>{line}<br /></>);
   return (
-    <section>
-      <h2>{fm.title}</h2>
-      <p>{excerptLines}</p>
-      <span>
+    <section className="post-short">
+      <Link to={fm.path}>
+        <h2 className="post-short-heading">{fm.title}</h2>
+      </Link>
+      <p className="post-short-excerpt">{excerptLines}</p>
+      <span className="post-short-meta">
         Posted on {fm.date} by {fm.author}
         {fm.bg && fm.bg !== "" ? " (" + fm.bg + ")" : ""}
       </span>
@@ -20,4 +24,14 @@ const PostShort = ({ node }) => {
   );
 };
 
-export default PostShort;
+const PostList = ({ edges }) => {
+  const Posts = edges.filter(edge => !edge.node.frontmatter.draft)
+                     .map(edge => <PostShort node={edge.node}/>)
+  return (
+    <div className="posts-container">
+      {Posts}
+    </div>
+  );
+};
+
+export default PostList;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,33 +4,27 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-import PostShort from "../components/posts.js"
+import PostList from "../components/posts.js"
 
-const IndexPage = ({ data: { allMarkdownRemark: { edges } } }) => {
-  const Posts = edges.filter(edge => !edge.node.frontmatter.draft)
-                     .map(edge => <PostShort node={edge.node}/>)
-  return (
-    <Layout>
-      <SEO title="Home" />
-      <section>
-        Das Basisdemokratische Bündnis ist eine Gruppe, in der sich
-        Einzelpersonen und diverse Basisgruppen und Fachgruppen zusammengefunden
-        haben, um gemeinsam emanzipatorische Politik an der Uni voran zu
-        bringen.
-      </section>
-      <div>
-        {Posts}
-      </div>
-    </Layout>
-  );
-};
+const IndexPage = ({ data: { allMarkdownRemark: { edges } } }) => (
+  <Layout>
+    <SEO title="Home" />
+    <section>
+      Das Basisdemokratische Bündnis ist eine Gruppe, in der sich
+      Einzelpersonen und diverse Basisgruppen und Fachgruppen zusammengefunden
+      haben, um gemeinsam emanzipatorische Politik an der Uni voran zu
+      bringen.
+    </section>
+    <PostList edges={edges} />
+  </Layout>
+);
 
 export const postQueryAll = graphql`
   query {
     allMarkdownRemark(limit: 10, sort: {fields: frontmatter___date, order: DESC}) {
       edges {
         node {
-          excerpt(format: MARKDOWN, pruneLength: 100)
+          excerpt(format: MARKDOWN, pruneLength: 200)
           frontmatter {
             path
             author
